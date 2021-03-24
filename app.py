05ce79cc615967defe4c0c775ed01bcf311fc0ac
Nicole Aragao <nicole.aragao@zoop.com.br>
2021-03-24 18:22:17 -0300

    created translate function

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ import json
 
 data = json.load(open('data.json'))
 
-word = input("Which word would you like to know? ")
 
-try:
-    meaning = data[word]
-    print(meaning)
+def translate():
+    word = input("Which word would you like to know? ")
+    try:
+        meaning = data[word]
+        print(meaning)
+    except:
+        print("The word was not found.")
 
-except:
-    print("The word was not found.")
+translate()
