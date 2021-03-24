a977be334a9351e7df4b16933cbd9bce59dc0cdf
Nicole Aragao <nicole.aragao@zoop.com.br>
2021-03-24 19:43:56 -0300

    expnding the dictionary, with the addition of a lib that looks for similar words in case the word collected by input initially cant be found

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open('data.json'))
 
 
 def translate():
-    word = input("Which word would you like to know? ")
-    try:
-        meaning = data[word]
-        print(meaning)
-    except:
-        print("The word was not found.")
+    word = input("Which word would you like to know? ").lower()
+    if word in data:
+        return data[word]
+    elif len(get_close_matches(word, data.keys(), cutoff=0.8)) > 0:
+        return "Did you mean %s instead?" % get_close_matches(word, data.keys(), cutoff=0.8) [0]
 
-translate()
+    else:
+        pass
+
+
+print(translate())
