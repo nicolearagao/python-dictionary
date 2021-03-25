199913c4ec454714e225a42c0a38ef9eefde5090
Nicole Aragao <nicole.aragao@zoop.com.br>
2021-03-25 14:56:41 -0300

    expansion of dictionary module with the addition of a mysql connector

--- dictionary.py
+++ dictionary.py
@@ -1,8 +1,18 @@
 from difflib import get_close_matches
+from db import connection
+
+
+def find_by_name(name):
+    cursor = connection.cursor()
+    query = cursor.execute("SELECT * FROM Dictionary where Expression=name")
+    results = cursor.fetchall()
+    return results
 
 
 def translate(data):
     word = input("Which word would you like to know? ").lower()
+
+    results = find_by_name(word)
 
     if word in data:
         return data[word]
@@ -15,7 +25,8 @@ def translate(data):
 
     elif len(get_close_matches(word, data.keys(), cutoff=0.8)) > 0:
         answer = input(
-            "Did you mean %s instead? Enter Y if yes, or N if no.\n " % get_close_matches(word, data.keys(), cutoff=0.8)[
+            "Did you mean %s instead? Enter Y if yes, or N if no.\n " %
+            get_close_matches(word, data.keys(), cutoff=0.8)[
                 0])[0].upper()
         if answer == "Y":
             return data[get_close_matches(word, data.keys(), cutoff=0.8)[0]]
